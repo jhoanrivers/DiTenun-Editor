54d1168f391260a3ced8c9d34bb916a8a6486e22
jhoanrivers <jhoanrivers@gmail.com>
2019-05-11 12:01:37 +0700

    tryagain

--- app/src/main/java/com/asksira/imagepickersheetdemo/activity/Sadum1Activity.java
+++ app/src/main/java/com/asksira/imagepickersheetdemo/activity/Sadum1Activity.java
@@ -54,6 +54,8 @@ public class Sadum1Activity extends AppCompatActivity implements View.OnTouchLis
     Button SelectMotifbtn,Ucapanbtn,ShowUcapanBtn;
     static boolean btnstatus=false;
 
+    ImageView img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15,img16,img17,img18,img19,img20,img21,img22,img23,img24,img25,img26,img27,img28,img29,img30,img31,img32,img33,img34,img35,img36,img37,img38,img39,img40,img41,img42,img43,img44,img45;
+
     private float mscaleFactor = 0.5f;
     private float mrotationDegree =0.f;
     private float mFocusX =0.f;
@@ -395,6 +397,36 @@ public class Sadum1Activity extends AppCompatActivity implements View.OnTouchLis
         ShowUcapanBtn = findViewById(R.id.btnshowucapan);
         textUcapan = findViewById(R.id.txt_ucapan);
         containerCenter = findViewById(R.id.containerCenter);
+        img1 = findViewById(R.id.imageView1);
+        img2 = findViewById(R.id.imageView1);
+        img3 = findViewById(R.id.imageView1);
+        img4 = findViewById(R.id.imageView1);
+        img5 = findViewById(R.id.imageView1);
+        img6 = findViewById(R.id.imageView1);
+        img7 = findViewById(R.id.imageView1);
+        img8 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
+        img1 = findViewById(R.id.imageView1);
 
 
     }
